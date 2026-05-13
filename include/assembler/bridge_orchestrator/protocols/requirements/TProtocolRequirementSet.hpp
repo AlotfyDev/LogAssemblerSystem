4c0ee86314e8d682957692d097c76d27827c5eca
Ahmed Lotfy <128230243+AlotfyDev@users.noreply.github.com>
2026-05-14 00:48:26 +0300

    ASCC-COMP-W05: remove duplicate participant requirement definition

--- include/assembler/bridge_orchestrator/protocols/requirements/TProtocolRequirementSet.hpp
+++ include/assembler/bridge_orchestrator/protocols/requirements/TProtocolRequirementSet.hpp
@@ -5,11 +5,9 @@
 #include <cstdint>
 #include <string_view>
 
-#include "assembler/communication_context/participants/roles/TParticipantRole.hpp"
-#include "assembler/communication_context/protocols/kinds/TBridgeProtocolKind.hpp"
 #include "assembler/communication_context/protocols/requirements/TProtocolBindingRequirement.hpp"
 #include "assembler/communication_context/protocols/requirements/TProtocolCarrierRequirement.hpp"
-#include "assembler/communication_context/protocols/requirements/TProtocolRequirementKind.hpp"
+#include "assembler/communication_context/protocols/requirements/TProtocolParticipantRequirement.hpp"
 
 /**
  * @file TProtocolRequirementSet.hpp
@@ -23,48 +21,6 @@
 
 namespace assembler::communication_context
 {
-    struct TProtocolParticipantRequirement final
-    {
-        std::uint64_t requirement_id{0u};
-        TBridgeProtocolKind protocol{TBridgeProtocolKind::unknown};
-        TParticipantRole required_role{TParticipantRole::unknown};
-        bool required{true};
-        std::string_view note{};
-
-        [[nodiscard]] constexpr bool is_valid() const noexcept
-        {
-            return requirement_id != 0u
-                && protocol != TBridgeProtocolKind::unknown
-                && required_role != TParticipantRole::unknown;
-        }
-
-        [[nodiscard]] constexpr bool is_required() const noexcept
-        {
-            return required;
-        }
-
-        [[nodiscard]] constexpr TProtocolRequirementKind requirement_kind() const noexcept
-        {
-            return TProtocolRequirementKind::participant_role_requirement;
-        }
-
-        [[nodiscard]] static constexpr TProtocolParticipantRequirement make(
-            std::uint64_t id,
-            TBridgeProtocolKind protocol_kind,
-            TParticipantRole role,
-            bool is_required_role = true,
-            std::string_view requirement_note = {}) noexcept
-        {
-            return TProtocolParticipantRequirement{
-                id,
-                protocol_kind,
-                role,
-                is_required_role,
-                requirement_note
-            };
-        }
-    };
-
     template <std::size_t ParticipantCapacity,
               std::size_t CarrierCapacity,
               std::size_t BindingCapacity>
