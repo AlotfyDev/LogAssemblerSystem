519176eb0c49da285dbdb4ca1897a3829618ad75
Ahmed Lotfy <128230243+AlotfyDev@users.noreply.github.com>
2026-05-14 02:56:25 +0300

    Add ASCC facade layer aggregate include

---
+++ include/assembler/bridge_orchestrator/facade_layer/TAsccFacadeLayer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "assembler/communication_context/facade_layer/common/TAsccComponentId.hpp"
+#include "assembler/communication_context/facade_layer/common/TAsccComponentVersion.hpp"
+#include "assembler/communication_context/facade_layer/common/TAsccFacadeSurfaceKind.hpp"
+
+#include "assembler/communication_context/facade_layer/administrative/TAsccAdministrativeFacade.hpp"
+#include "assembler/communication_context/facade_layer/administrative/TAsccComponentCapabilityCatalog.hpp"
+#include "assembler/communication_context/facade_layer/administrative/TAsccComponentDescriptor.hpp"
+#include "assembler/communication_context/facade_layer/administrative/TAsccComponentProfileId.hpp"
+
+#include "assembler/communication_context/facade_layer/managerial/TAsccComponentAdmission.hpp"
+#include "assembler/communication_context/facade_layer/managerial/TAsccComponentReadinessStatus.hpp"
+#include "assembler/communication_context/facade_layer/managerial/TAsccManagerialFacade.hpp"
+#include "assembler/communication_context/facade_layer/managerial/TAsccManagerialReadinessView.hpp"
+
+#include "assembler/communication_context/facade_layer/consuming/TAsccConsumingFacade.hpp"
+#include "assembler/communication_context/facade_layer/consuming/TAsccConsumingOperationView.hpp"
+#include "assembler/communication_context/facade_layer/consuming/TAsccConsumingResult.hpp"
+#include "assembler/communication_context/facade_layer/consuming/TAsccOperationMode.hpp"
+
+namespace assembler::communication_context
+{
+    inline constexpr bool ASCC_FACADE_LAYER_AVAILABLE = true;
+}
