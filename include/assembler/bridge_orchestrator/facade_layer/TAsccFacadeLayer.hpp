58955de7135b569f221b3b938dbe91a734cd06b8
Ahmed Lotfy <128230243+AlotfyDev@users.noreply.github.com>
2026-05-14 03:19:36 +0300

    Expose protocol-facing facade entities in ASCC facade aggregate

--- include/assembler/bridge_orchestrator/facade_layer/TAsccFacadeLayer.hpp
+++ include/assembler/bridge_orchestrator/facade_layer/TAsccFacadeLayer.hpp
@@ -8,11 +8,14 @@
 #include "assembler/communication_context/facade_layer/administrative/TAsccComponentCapabilityCatalog.hpp"
 #include "assembler/communication_context/facade_layer/administrative/TAsccComponentDescriptor.hpp"
 #include "assembler/communication_context/facade_layer/administrative/TAsccComponentProfileId.hpp"
+#include "assembler/communication_context/facade_layer/administrative/TAsccProtocolCatalogView.hpp"
 
 #include "assembler/communication_context/facade_layer/managerial/TAsccComponentAdmission.hpp"
 #include "assembler/communication_context/facade_layer/managerial/TAsccComponentReadinessStatus.hpp"
 #include "assembler/communication_context/facade_layer/managerial/TAsccManagerialFacade.hpp"
 #include "assembler/communication_context/facade_layer/managerial/TAsccManagerialReadinessView.hpp"
+#include "assembler/communication_context/facade_layer/managerial/TAsccProtocolAdmissionPolicy.hpp"
+#include "assembler/communication_context/facade_layer/managerial/TAsccProtocolReadinessView.hpp"
 
 #include "assembler/communication_context/facade_layer/consuming/TAsccConsumingFacade.hpp"
 #include "assembler/communication_context/facade_layer/consuming/TAsccConsumingOperationView.hpp"
@@ -22,4 +25,5 @@
 namespace assembler::communication_context
 {
     inline constexpr bool ASCC_FACADE_LAYER_AVAILABLE = true;
+    inline constexpr bool ASCC_PROTOCOL_FACADE_SURFACES_AVAILABLE = true;
 }
